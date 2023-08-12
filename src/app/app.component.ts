80345f20f98679fe8533e96f8c0521ce73a316aa
Dilek Baykara <baykara.d@gmail.com>
2023-08-12 19:27:31 -0400

    adding project files to Git

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Hello world!</h1>`,
+
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Real Estate App';
+}
+function useState(arg0: never[]): [any, any] {
+  throw new Error('Function not implemented.');
+}
+
+function useEffect(arg0: () => void, arg1: never[]) {
+  throw new Error('Function not implemented.');
+}
